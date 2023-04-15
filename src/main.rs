42059c16db91aa15c30621683625412e4ecd333f
Manuel Quarneti <manuelquarneti@gmail.com>
2023-04-15 14:57:59 +0200

    ➖ Remove futures

--- src/main.rs
+++ src/main.rs
@@ -33,37 +33,35 @@ fn main() -> Result<(), slint::PlatformError> {
         let ui_handle = ui_handle.clone();
 
         thread::spawn(move || {
-            futures_executor::block_on(async move {
-                let games = util::select_games();
-                let games_count = games.len() as i32;
+            let games = util::select_games();
+            let games_count = games.len() as i32;
 
+            let handle_weak = ui_handle.clone();
+            handle_weak
+                .upgrade_in_event_loop(move |handle_weak| {
+                    handle_weak.set_view("adding-games".into());
+                    handle_weak.set_max_progress(games_count);
+                })
+                .unwrap();
+
+            for (i, game) in games.iter().enumerate() {
                 let handle_weak = ui_handle.clone();
                 handle_weak
                     .upgrade_in_event_loop(move |handle_weak| {
-                        handle_weak.set_view("adding-games".into());
-                        handle_weak.set_max_progress(games_count);
+                        handle_weak.set_current_progress(i as i32 + 1);
                     })
                     .unwrap();
+                util::add_game(&drive_path, &game);
+            }
 
-                for (i, game) in games.iter().enumerate() {
-                    let handle_weak = ui_handle.clone();
-                    handle_weak
-                        .upgrade_in_event_loop(move |handle_weak| {
-                            handle_weak.set_current_progress(i as i32 + 1);
-                        })
-                        .unwrap();
-                    util::add_game(&drive_path, &game);
-                }
-
-                let handle_weak = ui_handle.clone();
-                handle_weak
-                    .upgrade_in_event_loop(move |handle_weak| {
-                        let games = util::get_games(&drive_path);
-                        handle_weak.set_view("games".into());
-                        handle_weak.set_games(games);
-                    })
-                    .unwrap();
-            });
+            let handle_weak = ui_handle.clone();
+            handle_weak
+                .upgrade_in_event_loop(move |handle_weak| {
+                    let games = util::get_games(&drive_path);
+                    handle_weak.set_view("games".into());
+                    handle_weak.set_games(games);
+                })
+                .unwrap();
         });
     });
 
