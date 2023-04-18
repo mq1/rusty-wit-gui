35eb536abb5f421b27c88892f8592849fcc67ce8
Manuel Quarneti <manuelquarneti@gmail.com>
2023-04-18 18:19:43 +0200

    ✨ Automatic wit download

--- src/util.rs
+++ src/util.rs
@@ -4,9 +4,9 @@
 use crate::Game;
 
 use std::{
-    fs,
+    fs::{self, File},
     path::{Path, PathBuf},
-    process::Command,
+    process::Command, io,
 };
 
 use anyhow::Result;
@@ -14,13 +14,74 @@ use ini::Ini;
 use rfd::{FileDialog, MessageButtons, MessageDialog};
 use slint::{ModelRc, VecModel};
 
+pub fn download_wit(drive_mount_point: &str) -> Result<()> {
+    let file_name = if cfg!(target_os = "macos") {
+        "wit-v3.04a-r8427-mac.tar.gz"
+    } else if cfg!(target_os = "windows") {
+        "wit-v3.04a-r8427-cygwin64.zip"
+    } else {
+        "wit-v3.04a-r8427-x86_64.tar.gz"
+    };
+
+    let download_path = Path::new(drive_mount_point).join(file_name);
+    let download_url = format!("https://wit.wiimm.de/download/{file_name}");
+
+    let mut body = ureq::get(&download_url).call()?.into_reader();
+    let mut file = File::create(&download_path)?;
+    io::copy(&mut body, &mut file)?;
+
+    if cfg!(target_family = "unix") {
+        let output = Command::new("tar")
+            .arg("xzf")
+            .arg(&download_path)
+            .arg("-C")
+            .arg(drive_mount_point)
+            .output()?;
+        println!("{:?}", output);
+    } else {
+        let output = Command::new("powershell")
+            .arg("Expand-Archive")
+            .arg(&download_path)
+            .arg(drive_mount_point)
+            .output()?;
+        println!("{:?}", output);
+    }
+
+    fs::remove_file(&download_path)?;
+
+    Ok(())
+}
+
+pub fn get_wit_path(drive_mount_point: &str) -> Result<PathBuf> {
+    let base_dir = if cfg!(target_os = "macos") {
+        Path::new(drive_mount_point).join("wit-v3.04a-r8427-mac")
+    } else if cfg!(target_os = "windows") {
+        Path::new(drive_mount_point).join("wit-v3.04a-r8427-cygwin64")
+    } else {
+        Path::new(drive_mount_point).join("wit-v3.04a-r8427-x86_64")
+    };
+
+    let wit_path = if cfg!(target_family = "unix") {
+        base_dir.join("bin").join("wit")
+    } else {
+        base_dir.join("bin").join("wit.exe")
+    };
+
+    Ok(wit_path)
+}
+
 pub fn get_games(drive_mount_point: &str) -> Result<ModelRc<Game>> {
     let wbfs_folder = Path::new(drive_mount_point).join("wbfs");
     if !wbfs_folder.exists() {
         fs::create_dir(&wbfs_folder)?;
     }
 
-    let output = Command::new("wit")
+    let wit_path = get_wit_path(drive_mount_point)?;
+    if !wit_path.exists() {
+        download_wit(drive_mount_point)?;
+    }
+
+    let output = Command::new(wit_path)
         .arg("list")
         .arg(wbfs_folder)
         .arg("--sections")
@@ -63,7 +124,9 @@ pub fn select_games() -> Vec<PathBuf> {
 }
 
 pub fn add_game(drive_mount_point: &str, game: &Path) -> Result<()> {
-    let output = Command::new("wit").arg("id6").arg(&game).output()?;
+    let wit_path = get_wit_path(drive_mount_point)?;
+
+    let output = Command::new(&wit_path).arg("id6").arg(&game).output()?;
     let game_id = String::from_utf8(output.stdout)?.trim().to_string();
 
     let path = Path::new(drive_mount_point)
@@ -71,7 +134,7 @@ pub fn add_game(drive_mount_point: &str, game: &Path) -> Result<()> {
         .join(game_id)
         .with_extension("wbfs");
 
-    let output = Command::new("wit")
+    let output = Command::new(wit_path)
         .arg("copy")
         .arg(&game)
         .arg(&path)
@@ -92,7 +155,9 @@ pub fn remove_game(drive_mount_point: &str, game: &Game) -> Result<ModelRc<Game>
         .show();
 
     if yes {
-        let output = Command::new("wit")
+        let wit_path = get_wit_path(drive_mount_point)?;
+
+        let output = Command::new(wit_path)
             .arg("remove")
             .arg(game.path.as_str())
             .output()?;
